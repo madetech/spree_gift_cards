037e887e58b35d12739abf882653a00d8ad1f243
Rhys Stansfield <r.stansfield@voc-8.com>
2015-02-13 18:15:31 +0000

    Change included block to regular self.included - should fix ActiveSupport::Concern multiple included blocks error

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,24 +1,16 @@
 module SpreeGiftCards::OrderDecorator
-  extend ActiveSupport::Concern
-
-  included do
-    prepend(InstanceMethods)
+  def finalize!
+    create_gift_cards
+    super
   end
 
-  module InstanceMethods
-    def finalize!
-      create_gift_cards
-      super
-    end
-
-    def create_gift_cards
-      line_items.each do |item|
-        item.quantity.times do
-          Spree::VirtualGiftCard.create!(amount: item.price, currency: item.currency, purchaser: user, line_item: item) if item.gift_card?
-        end
+  def create_gift_cards
+    line_items.each do |item|
+      item.quantity.times do
+        Spree::VirtualGiftCard.create!(amount: item.price, currency: item.currency, purchaser: user, line_item: item) if item.gift_card?
       end
     end
   end
 end
 
-Spree::Order.include(SpreeGiftCards::OrderDecorator)
+Spree::Order.prepend(SpreeGiftCards::OrderDecorator)
